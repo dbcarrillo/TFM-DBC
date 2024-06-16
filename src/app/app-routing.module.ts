7836bc286c617e3742da6278e2cc89f4574de054
[Daniel Benito Carrillo] <[dbcarrillo@uoc.edu]>
2024-06-16 08:41:08 +0200

    Edición enrutado, estilos globales e instalación de paquetes: 	modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/index.html 	modified:   src/styles.css 	modified:   tsconfig.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticuloEjemploComponent } from './pages/articulo-ejemplo/articulo-ejemplo.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { EventoEjemploComponent } from './pages/evento-ejemplo/evento-ejemplo.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { TerminosCookiesPrivacidadComponent } from './pages/terminos-cookies-privacidad/terminos-cookies-privacidad.component';
+import { TiburonEjemploComponent } from './pages/tiburon-ejemplo/tiburon-ejemplo.component';
+import { TiburonpediaComponent } from './pages/tiburonpedia/tiburonpedia.component';
 
-const routes: Routes = [];
+// Rutas navegación
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'quienes-somos', component: QuienesSomosComponent },
+  { path: 'tiburonpedia', component: TiburonpediaComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'articulo-ejemplo', component: ArticuloEjemploComponent },
+  { path: 'evento-ejemplo', component: EventoEjemploComponent },
+  { path: 'tiburon-ejemplo', component: TiburonEjemploComponent },
+  {
+    path: 'terminos-cookies-privacidad',
+    component: TerminosCookiesPrivacidadComponent,
+  },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'ignore',
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ArticuloFichaComponent } from './items/components/articulo-ficha/articulo-ficha.component';
 import { ArticuloListaComponent } from './items/components/articulo-lista/articulo-lista.component';
@@ -16,7 +22,6 @@ import { OpcionesContactoComponent } from './items/components/opciones-contacto/
 import { TiburonFichaComponent } from './items/components/tiburon-ficha/tiburon-ficha.component';
 import { TiburonListaComponent } from './items/components/tiburon-lista/tiburon-lista.component';
 import { TomarAccionComponent } from './items/components/tomar-accion/tomar-accion.component';
-import { EstadoTiburonnPipe } from './pipes/estado-tiburonn.pipe';
 import { ArticuloEjemploComponent } from './pages/articulo-ejemplo/articulo-ejemplo.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
@@ -28,41 +33,49 @@ import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.compo
 import { TerminosCookiesPrivacidadComponent } from './pages/terminos-cookies-privacidad/terminos-cookies-privacidad.component';
 import { TiburonEjemploComponent } from './pages/tiburon-ejemplo/tiburon-ejemplo.component';
 import { TiburonpediaComponent } from './pages/tiburonpedia/tiburonpedia.component';
+import { EstadoTiburonnPipe } from './pipes/estado-tiburonn.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticuloFichaComponent,
-    ArticuloListaComponent,
-    CarouselComponent,
-    DatosComponent,
-    FaqsComponent,
+    TiburonpediaComponent,
+    EventosComponent,
+    ContactoComponent,
+    BlogComponent,
+    QuienesSomosComponent,
+    HomeComponent,
     FooterComponent,
-    FormComponent,
-    MasInfoComponent,
     NavbarComponent,
-    OpcionesContactoComponent,
+    FormComponent,
+    TomarAccionComponent,
+    FaqsComponent,
+    DatosComponent,
     TiburonFichaComponent,
     TiburonListaComponent,
-    TomarAccionComponent,
     EstadoTiburonnPipe,
+    CarouselComponent,
+    ArticuloFichaComponent,
+    ArticuloListaComponent,
     ArticuloEjemploComponent,
-    BlogComponent,
-    ContactoComponent,
     EventoEjemploComponent,
-    EventosComponent,
-    HomeComponent,
-    NotfoundComponent,
-    QuienesSomosComponent,
-    TerminosCookiesPrivacidadComponent,
     TiburonEjemploComponent,
-    TiburonpediaComponent
+    TerminosCookiesPrivacidadComponent,
+    NotfoundComponent,
+    MasInfoComponent,
+    OpcionesContactoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule.forRoot(),
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
